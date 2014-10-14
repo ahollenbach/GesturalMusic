569d9da183b4c8e0d3915dfb62aa279e266e6f25
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2014-10-14 18:54:03 -0400

    Cleaned up test file

--- Other/OSCtest.js
+++ Other/OSCtest.js
@@ -1,11 +1,9 @@
+// Requires OSC Receiver (https://www.npmjs.org/package/osc-receiver)
+
 var OscReceiver = require('osc-receiver'), receiver = new OscReceiver();
 console.log("OSC receiver started...");
 receiver.bind(8000);
 
 receiver.on('/test', function(a) {
 	console.log(a);
-});
-
-receiver.on('message', function() {
-	//console.log(arguments);
 });
