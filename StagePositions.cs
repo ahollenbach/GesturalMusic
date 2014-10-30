d9a8507b0e1ab553b6afef357bfb182f5217f0da
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2014-10-30 12:05:12 -0400

    Enum types for partitioning. [not complete]

---
+++ StagePositions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Samples.Kinect.BodyBasics
+{
+    enum PartitionType
+    {
+        Single,
+        DoubleLeftRight,
+        DoubleFrontBack,
+        Quad
+    }
+
+    // facing the Kinect
+    enum Partition
+    {
+        Left,
+        Right,
+        Front,
+        Back
+    }
+}
