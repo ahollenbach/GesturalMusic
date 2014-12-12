922980102ab990393f3ce71da40e9ef27f3bcd8d
vraman3 <vishwanath.raman3@gmail.com>
2014-12-11 21:42:11 -0500

    LooperOSC

---
+++ LooperOSC.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventuz.OSC;
+
+namespace GesturalMusic
+{ 
+    class LooperOSC
+    {
+        private string name;
+        private UdpWriter osc;
+
+
+    }
+}
