30b4425f055e838f4fef55108bca25f3d36a4ab9
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2015-03-16 15:23:23 -0400

    Working X and Y filters

--- Filter.cs
+++ Filter.cs
@@ -21,12 +21,19 @@ namespace GesturalMusic
         {
             CameraSpacePoint rWrist    = body.Joints[JointType.WristRight].Position;
             CameraSpacePoint rShoulder = body.Joints[JointType.ShoulderRight].Position;
+            CameraSpacePoint head      = body.Joints[JointType.Head].Position;
 
-            // X works fairly well
-            // Y is somewhat bad
-            // Z is bad
-            MainWindow.osc.Send(new OscElement("/" + instrumentName + "/filterX", rWrist.X - rShoulder.X));
-            MainWindow.osc.Send(new OscElement("/" + instrumentName + "/filterY", rWrist.Y));
+            double forearmLength = Utils.Length(body.Joints[JointType.WristRight], body.Joints[JointType.ElbowRight]);
+            double xDist = rWrist.X - rShoulder.X;
+            double yDist = rWrist.Y - head.Y;
+
+            // .13 -> .4 ( roughly .25)
+            // .25 ( .13/.25 ~= 0.5)
+
+            // .88 / .58 / -.15   (.3, .75)
+            Console.WriteLine(Math.Pow(1 + head.Y - 3 * forearmLength + yDist, .3));
+            MainWindow.osc.Send(new OscElement("/" + instrumentName + "/filterX", (float) ((xDist - .5 * forearmLength) * 4)));
+            MainWindow.osc.Send(new OscElement("/" + instrumentName + "/filterY", (float) Math.Pow(1 + head.Y - 3 * forearmLength + yDist, 0.3)));
             MainWindow.osc.Send(new OscElement("/" + instrumentName + "/filterZ", rShoulder.Z - rWrist.Z));
         }
     }
